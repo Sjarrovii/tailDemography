d48c3f6f6b76925b8d2a458968e53c7e690c37c1
Christopher Agard <cragard@gmail.com>
2018-09-18 23:28:52 -0400

    Added separate packages
    Added packages, liztoes and liznum in preparation for first PyPi release.

---
+++ python notebooks/functions/venv/liztoes/make_str.py
@@ -0,0 +1,10 @@
+import pandas as pd
+def make_str(x):
+    assert isinstance(x,pd.Series)
+    #convert series to string
+    x = x.astype(str)
+    #create an index of single-digit numbers
+    idx = x.str.len()<2
+    #add a zero to the beginning of those single-digit numbers
+    x.loc[idx] = '0' + x.loc[idx]
+    return x
