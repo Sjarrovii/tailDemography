cd44b91c2930a3a7af0dd1083a731d43938031c9
Christopher Agard <cragard@gmail.com>
2018-12-12 01:19:19 -0500

    Began writing Cleaning (Part 1)
    I stopped at the combining synonyms section

--- python notebooks/liz_number.py
+++ python notebooks/liz_number.py
@@ -2,19 +2,40 @@ import os
 import pandas as pd
 
 
-def validate(x, sort_criteria=['species', 'toes', 'sex'], validation=['date', 'svl']):
+def validate(x, sort_criteria=['species', 'toes', 'sex'], validation=['year_diff <= 7','svl_diff >= -2']):
+    """
+    Takes a pandas data frame and creates a new column in the data set populated by the number 1.  It trims the df so
+    that to only retains those rows for which validation criteria are satisfied. Validate then groups by sort_criteria,
+    produces the minimum value for tmp in each group before assigning the cummulative sum as the new tmp value in each
+    row. It produces df of rows containing non-unique individuals as well as counts of unvalidated and validated rows in
+    the original df.  Validate uses merging to assign the cummulative sums as liznumber in the original data set
+    It then drop 'tmp' and 'smallest_svl, prints summary results, and returns a dict of the original df, and counts of
+    validated and unvalidated rows in a dict.
+
+    :param x: 
+    :param sort_criteria: 
+    :param validation: 
+    :return: 
+    """
+    #Create a new column in the data set populated by the number
     x['tmp'] = 1
+    #Trim that to only retain those rows for which validation criteria are satisfied. Group by sort_criteria and produce
+    #the minimum value for tmp in each group. Assign the cummulative sum as the new tmp value in each row.
     numbers = x.loc[(x.year_diff <= 7) & (x.svl_diff >= -2), :]. \
-        groupby(['species', 'sex', 'toes']).tmp.min().cumsum().reset_index()
-    validated = x.loc[(x.year_diff <= 7) & (x.svl_diff >= -2), :].shape[0]
-    not_val_data = x.loc[(x.year_diff <= 7) & (x.svl_diff >= -2), :]
+        groupby(sort_criteria).tmp.min().cumsum().reset_index()
+    #Produce the a df of rows which passed validation criteria (i.e. non-unique individuals)
+    not_val_data = x.loc[~((x.year_diff <= 7) & (x.svl_diff >= -2)), :]
+    #Produce counts of unvalidated and validated rows
     not_validated = x.loc[~((x.year_diff <= 7) & (x.svl_diff >= -2)), :].shape[0]
-    numbers = numbers.rename(columns={'tmp': 'liznumber'})  # rename last column to liznumber
-    # the next line merges the numbers to the original data frame to assign the lizard number to the full record
-    # of an animal.  It then drop 'tmp'and 'smallest_svl, since we won't be using these again
-    x = x.merge(numbers, 'left', on=['species', 'sex', 'toes']).drop(['tmp', 'smallest_svl'], 1)
+    #Rename last column to liznumber
+    numbers = numbers.rename(columns={'tmp': 'liznumber'})
+    #the next line merges the numbers to the original data frame to assign the lizard number to the full record
+    #of an animal.  It then drop 'tmp' and 'smallest_svl, since we won't be using these again
+    x = x.merge(numbers, 'left', on=sort_criteria).drop(['tmp', 'smallest_svl'], 1)
+    #Print summary results
     print("\nOf those entries we can handle, there are {} individuals as defined by {} which pass validation based\
-    on {} and {} which do not pass validation.".format(validated, sort_criteria, validation, not_validated))
+    on {} and {} rows which do not pass validation.".format(x.liznumber.max(), sort_criteria, validation, not_validated))
+    #Return dict of the original df,
     return {'val_data': x, 'n_val_data': not_val_data, 'n_validated': not_validated}
 
 
@@ -47,8 +68,8 @@ def mindate(x, sort_criteria = ['species', 'toes', 'sex']):
 def lizsort(x, path: str, sort_criteria = ['species', 'toes', 'sex'], validation = ['date', 'svl'],\
             unsortablefile='unsortable.csv'):
     """
-    takes a pandas data frame and returns a pandas dataframe with only those values which
-    can be evaluated according to given criteria and prints a summaryof the files evaluated
+    takes a pandas data frame and returns a pandas data frame with only those values which
+    can be evaluated according to given criteria and prints a summary of the files evaluated
     :param path:
     :param sortCriteria:
     :param validation:
