a6fbbf741941e449e31f25460c32eee43ca27051
Christopher Agard <cragard@gmail.com>
2019-03-11 00:34:34 -0400

    minor changes to structure of library

--- python notebooks/liz_number.py
+++ python notebooks/liz_number.py
@@ -2,7 +2,8 @@ import os
 import pandas as pd
 
 
-def validate(x, sort_criteria=['species', 'toes', 'sex'], validation=['year_diff <= 7','svl_diff >= -2']):
+def validate(x, sort_criteria=['species', 'toes', 'sex','initialCaptureDate','smallest_svl'], 
+             validation=['year_diff <= 7','svl_diff >= -2']):
     """
     Takes a pandas data frame and creates a new column in the data set populated by the number 1.  It trims the df so
     that to only retains those rows for which validation criteria are satisfied. Validate then groups by sort_criteria,
@@ -12,31 +13,32 @@ def validate(x, sort_criteria=['species', 'toes', 'sex'], validation=['year_diff
     It then drop 'tmp' and 'smallest_svl, prints summary results, and returns a dict of the original df, and counts of
     validated and unvalidated rows in a dict.
 
+    :type validation: list
     :param x: 
     :param sort_criteria: 
     :param validation: 
     :return: 
     """
-    #Create a new column in the data set populated by the number
+    #Create a new column in the data set populated by the number 1
     x['tmp'] = 1
     #Trim that to only retain those rows for which validation criteria are satisfied. Group by sort_criteria and produce
     #the minimum value for tmp in each group. Assign the cummulative sum as the new tmp value in each row.
     numbers = x.loc[(x.year_diff <= 7) & (x.svl_diff >= -2), :]. \
         groupby(sort_criteria).tmp.min().cumsum().reset_index()
     #Produce the a df of rows which passed validation criteria (i.e. non-unique individuals)
-    not_val_data = x.loc[~((x.year_diff <= 7) & (x.svl_diff >= -2)), :]
+    not_val_df = x.loc[~((x.year_diff <= 7) & (x.svl_diff >= -2)), :]
     #Produce counts of unvalidated and validated rows
-    not_validated = x.loc[~((x.year_diff <= 7) & (x.svl_diff >= -2)), :].shape[0]
+    not_validated_count = x.loc[~((x.year_diff <= 7) & (x.svl_diff >= -2)), :].shape[0]
     #Rename last column to liznumber
-    numbers = numbers.rename(columns={'tmp': 'liznumber'})
+    numbers = numbers.rename(columns = {'tmp': 'liznumber'})
     #the next line merges the numbers to the original data frame to assign the lizard number to the full record
-    #of an animal.  It then drop 'tmp' and 'smallest_svl, since we won't be using these again
-    x = x.merge(numbers, 'left', on=sort_criteria).drop(['tmp', 'smallest_svl'], 1)
+    #of an animal.  It then drop 'tmp', since we won't be using this again
+    x = x.merge(numbers, 'left', on = sort_criteria).drop(['tmp'], 1)
     #Print summary results
     print("\nOf those entries we can handle, there are {} individuals as defined by {} which pass validation based\
-    on {} and {} rows which do not pass validation.".format(x.liznumber.max(), sort_criteria, validation, not_validated))
+    on {} and {} rows which do not pass validation.".format(x.liznumber.max(), sort_criteria, validation, not_validated_count))
     #Return dict of the original df,
-    return {'val_data': x, 'n_val_data': not_val_data, 'n_validated': not_validated}
+    return {'val_data': x, 'n_val_data': not_val_df, 'n_validated': not_validated_count}
 
 
 def smallest(x, svlGroup =['species', 'toes', 'sex', 'initialCaptureDate']):
@@ -59,19 +61,19 @@ def mindate(x, sort_criteria = ['species', 'toes', 'sex']):
     """
     if any(x.columns == 'initialCaptureDate'):
         x = x.drop('initialCaptureDate',1)
-    sortable_min_date =pd.DataFrame(x.groupby(sort_criteria).date.min()).\
+    sortable_min_date = pd.DataFrame(x.groupby(sort_criteria).date.min()).\
         rename(index=str, columns={'date': 'initialCaptureDate'}).reset_index()
     x = x.merge(sortable_min_date, how='left', on=sort_criteria)
     x['year_diff'] = x.date.dt.year - x.initialCaptureDate.dt.year
     return x
 
 def lizsort(x, path: str, sort_criteria = ['species', 'toes', 'sex'], validation = ['date', 'svl'],\
-            unsortablefile='unsortable.csv'):
+            unsortablefile = 'unsortable.csv'):
     """
     takes a pandas data frame and returns a pandas data frame with only those values which
     can be evaluated according to given criteria and prints a summary of the files evaluated
     :param path:
-    :param sortCriteria:
+    :param sort_criteria:
     :param validation:
     :param unsortablefile:
     """
