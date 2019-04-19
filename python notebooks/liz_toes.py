c9e62fa0c9209f132f29143d8b9063ed0901027f
Christopher Agard <cragard@gmail.com>
2019-04-19 02:42:48 -0400

    debugging cleaning part 2 notebook

--- python notebooks/liz_toes.py
+++ python notebooks/liz_toes.py
@@ -5,7 +5,10 @@ import numpy as np
 def label_pattern(x, pat_num, pattern, pat_col='toe_pattern', col='toes'):
     """searches a pandas series for a regex expression, pattern, and replaces with replacement"""
     # To-Do needs to capture when an entry fits multiple patterns and which patterns those are
-    x.loc[(x[col].str.match(pattern) is True) & (x[pat_col].isnull()), pat_col] = str(pat_num)
+    try:
+        x.loc[(x[col].str.match(pattern) is True) & (x[pat_col].isnull()), pat_col] = str(pat_num)
+    except Exception as e:
+        logging.error("No values in f'{col} match f'{pattern} where f'{pat_col} is null")
     return x
 
 
