7eeb5494d67f65627ac66ddf4ca798ddd20a42a8
Christopher Agard <cragard@gmail.com>
2019-04-21 23:38:56 -0400

    Fixed bug in label_pattern

--- python notebooks/liz_toes.py
+++ python notebooks/liz_toes.py
@@ -6,7 +6,7 @@ def label_pattern(x, pat_num, pattern, pat_col='toe_pattern', col='toes'):
     """searches a pandas series for a regex expression, pattern, and replaces with replacement"""
     # To-Do needs to capture when an entry fits multiple patterns and which patterns those are
     try:
-        x.loc[(x[col].str.match(pattern) is True) & (x[pat_col].isnull()), pat_col] = str(pat_num)
+        x.loc[(x[col].str.match(pattern)) & (x[pat_col].isnull()), pat_col] = str(pat_num)
     except Exception as e:
         logging.error("No values in f'{col} match f'{pattern} where f'{pat_col} is null")
     return x
