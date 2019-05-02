577419319d1ab2c909e04d3631e4eb44ec539bd3
Christopher Agard <cragard@gmail.com>
2019-05-02 01:10:46 -0400

    fixed validate and added logging to lizsort and validate functions
    fixed validate function to avoid merge issues when calling this function on dfs that already have a 'liznumber' column.
    
    added logging to replace print statements in lizsort and validate functions

--- python notebooks/liz_number.py
+++ python notebooks/liz_number.py
@@ -1,9 +1,10 @@
 import os
 import pandas as pd
+import logging
 
 
-def validate(x, sort_criteria=['species', 'toes', 'sex','initialCaptureDate','smallest_svl'], 
-             validation=['year_diff <= 7','svl_diff >= -2']):
+def validate(x, sort_criteria=['species', 'toes', 'sex', 'initialCaptureDate', 'smallest_svl'],
+             validation=['year_diff <= 7', 'svl_diff >= -2']):
     """
     Takes a pandas data frame and creates a new column in the data set populated by the number 1.  It trims the df so
     that to only retains those rows for which validation criteria are satisfied. Validate then groups by sort_criteria,
@@ -19,41 +20,46 @@ def validate(x, sort_criteria=['species', 'toes', 'sex','initialCaptureDate','sm
     :param validation: 
     :return: 
     """
-    #Create a new column in the data set populated by the number 1
+    # Drop liznumber if it already exists in x
+    if 'liznumber' in x.columns:
+        x = x.drop('liznumber',1)
+    # Create a new column in the data set populated by the number 1
     x['tmp'] = 1
-    #Trim that to only retain those rows for which validation criteria are satisfied. Group by sort_criteria and produce
-    #the minimum value for tmp in each group. Assign the cummulative sum as the new tmp value in each row.
+    # Trim so that we only retain those rows for which validation criteria are satisfied. Group by sort_criteria and
+    # produce the minimum value for tmp in each group. Assign the cumulative sum as the new tmp value in each row.
     numbers = x.loc[(x.year_diff <= 7) & (x.svl_diff >= -2), :]. \
         groupby(sort_criteria).tmp.min().cumsum().reset_index()
-    #Produce the a df of rows which passed validation criteria (i.e. non-unique individuals)
+    # Rename last column to liznumber
+    numbers = numbers.rename(columns={'tmp': 'liznumber'})
+    # the next line merges the numbers to the original data frame to assign the lizard number to the full record
+    # of an animal.  It then drop 'tmp', since we won't be using this again
+    x = x.merge(numbers, 'left', on=sort_criteria).drop(['tmp'], 1)
+    # Produce the a df of rows which passed validation criteria (i.e. non-unique individuals)
     not_val_df = x.loc[~((x.year_diff <= 7) & (x.svl_diff >= -2)), :]
-    #Produce counts of unvalidated and validated rows
-    not_validated_count = x.loc[~((x.year_diff <= 7) & (x.svl_diff >= -2)), :].shape[0]
-    #Rename last column to liznumber
-    numbers = numbers.rename(columns = {'tmp': 'liznumber'})
-    #the next line merges the numbers to the original data frame to assign the lizard number to the full record
-    #of an animal.  It then drop 'tmp', since we won't be using this again
-    x = x.merge(numbers, 'left', on = sort_criteria).drop(['tmp'], 1)
-    #Print summary results
-    print("\nOf those entries we can handle, there are {} individuals as defined by {} which pass validation based\
-    on {} and {} rows which do not pass validation.".format(x.liznumber.max(), sort_criteria, validation, not_validated_count))
-    #Return dict of the original df,
+    # Produce counts of unvalidated and validated rows
+    not_validated_count = not_val_df.shape[0]
+    # print summary results
+    logging.info("\nOf those entries we can handle, there are {} individuals as defined by {} which pass validation based\
+    on {} and {} rows which do not pass validation.".format(x.liznumber.max(), sort_criteria, validation,
+                                                            not_validated_count))
+    # Return dict of the original df, the number of unvalidated individuals, and the number of validated individuals
     return {'val_data': x, 'n_val_data': not_val_df, 'n_validated': not_validated_count}
 
 
-def smallest(x, svlGroup =['species', 'toes', 'sex', 'initialCaptureDate']):
+def smallest(x, svl_group=['species', 'toes', 'sex', 'initialCaptureDate']):
     """finds svl of animal at date of the initial capture."""
     # To-Do:  Make 'sortable_smallest_svl' into local variable
     if any(x.columns == 'smallest_svl'):
         x = x.drop('smallest_svl', 1)
-    sortable_smallest_svl = x.groupby(svlGroup).svl.min().reset_index()\
+    sortable_smallest_svl = x.groupby(svl_group).svl.min().reset_index()\
         .rename(index=str, columns={'svl': 'smallest_svl'})
     # sortable_smallest_svl
-    x = x.merge(sortable_smallest_svl, how='left', on=svlGroup)
+    x = x.merge(sortable_smallest_svl, how='left', on=svl_group)
     x['svl_diff'] = x.svl - x.smallest_svl
     return x
 
-def mindate(x, sort_criteria = ['species', 'toes', 'sex']):
+
+def mindate(x, sort_criteria=['species', 'toes', 'sex']):
     """
     takes a pandas data frame and returns a dataframe with sorting criteria adds a column containing the earliest date
     at which each unique combination of the sort criteria was sighted. [Requires that the source dataframe,x, has a
@@ -67,8 +73,9 @@ def mindate(x, sort_criteria = ['species', 'toes', 'sex']):
     x['year_diff'] = x.date.dt.year - x.initialCaptureDate.dt.year
     return x
 
-def lizsort(x, path: str, sort_criteria = ['species', 'toes', 'sex'], validation = ['date', 'svl'],\
-            unsortablefile = 'unsortable.csv'):
+
+def lizsort(x, path: str, sort_criteria=['species', 'toes', 'sex'], validation=['date', 'svl'],\
+            unsortablefile='unsortable.csv'):
     """
     takes a pandas data frame and returns a pandas data frame with only those values which
     can be evaluated according to given criteria and prints a summary of the files evaluated
@@ -84,7 +91,7 @@ def lizsort(x, path: str, sort_criteria = ['species', 'toes', 'sex'], validation
     sortable = x.loc[x.loc[:, critical].notnull().all(axis=1)]
     os.chdir(path)
     unsortable.to_csv(unsortablefile)
-    print("\nThere were {} entries for which values for one of the critical criteria, ({}), were null.  \
-    These entries could not be evaluated and were written out to the file {} for evaluation."\
-          .format(unsortable.shape[0], critical, unsortablefile))
+    logging.info("\nThere were {} entries for which values for one of the critical criteria, ({}), were null.  \
+    These entries could not be evaluated and were written out to the file {} for evaluation."
+                 .format(unsortable.shape[0], critical, unsortablefile))
     return sortable
